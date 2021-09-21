05a3b651af64b2a66cd6203b8684d5451de8553a
Nate McNamara <nate@mcnamara.net>
2021-09-21 08:30:45 -0400

    Use arenas for protobuf objects

--- src/pb.cpp
+++ src/pb.cpp
@@ -12,6 +12,7 @@
 #include <stdexcept>
 #include <string>
 
+using google::protobuf::Arena;
 using namespace std::literals;
 
 namespace {
@@ -20,13 +21,14 @@ namespace {
     {
         assert(message.size() <= std::numeric_limits<int>::max());
 
-        Response r;
-        if (!r.ParseFromArray(message.data(), static_cast<int>(message.size())))
+        Arena arena;
+        Response* const r = Arena::Create<Response>(&arena);
+        if (!r->ParseFromArray(message.data(), static_cast<int>(message.size())))
             throw std::runtime_error("Bad message received");
-        switch (r.Reply_case()) {
-        case Response::ReplyCase::kGot : return os << r.got().value();
+        switch (r->Reply_case()) {
+        case Response::ReplyCase::kGot : return os << r->got().value();
         case Response::ReplyCase::kSot : return os << "Done.";
-        case Response::ReplyCase::kFail: return os << r.fail().message();
+        case Response::ReplyCase::kFail: return os << r->fail().message();
         default                        : throw std::runtime_error("Bad message");
         }
         return os;
@@ -51,10 +53,11 @@ namespace {
 
     TEST_CASE("pb_deserialize_response(pb_serialize(Response)) is identity") {
         const auto v = "who's there?"s;
-        Response r;
-        r.mutable_got()->set_value(v);
+        Arena arena;
+        Response* const r = Arena::Create<Response>(&arena);
+        r->mutable_got()->set_value(v);
         char buf[4096];
-        const auto len = pb_serialize(std::span(buf, buf + sizeof buf), r);
+        const auto len = pb_serialize(std::span(buf, buf + sizeof buf), *r);
         CHECK(len > 0);
         std::ostringstream os;
         pb_deserialize_response(os, {buf, static_cast<std::size_t>(len)});
@@ -62,9 +65,10 @@ namespace {
     }
 
     int32_t pb_serialize_request(std::span<char> dest, std::string_view command) {
-        Request r;
+        Arena arena;
+        Request* const r = Arena::Create<Request>(&arena);
         if (const auto [cmd, args] = which_command(command); cmd == Command::get) {
-            *r.mutable_get()->mutable_key() = args;
+            *r->mutable_get()->mutable_key() = args;
         } else if (cmd == Command::set) {
             // TODO Consider doing the parsing inline to be more efficient
             if (const auto params = parse_set_args(args); params.empty()) {
@@ -72,7 +76,7 @@ namespace {
                 return -1;
             } else {
                 for (const auto& [key, value]: params) {
-                    const auto m = r.mutable_set()->add_mapping();
+                    const auto m = r->mutable_set()->add_mapping();
                     *m->mutable_key() = key;
                     *m->mutable_value() = value;
                 }
@@ -82,7 +86,7 @@ namespace {
             //throw std::runtime_error("Invalid command");
         }
 
-        return pb_serialize(dest, r);
+        return pb_serialize(dest, *r);
     }
 
     TEST_CASE("pb_serialize_request does") {
@@ -107,34 +111,35 @@ int32_t PbClientSerdes::serialize_request(
 int32_t pb_process_request(std::span<char> dest, std::string_view message, KV& kv) {
     assert(message.size() <= std::numeric_limits<int>::max());
 
-    Request in;
-    if (!in.ParseFromArray(message.data(), static_cast<int>(message.size())))
+    Arena arena;
+    Request* const in = Arena::Create<Request>(&arena);
+    if (!in->ParseFromArray(message.data(), static_cast<int>(message.size())))
         throw std::runtime_error("Bad message received");
-    Response out;
-    switch (in.Command_case()) {
+    Response* const out = Arena::Create<Response>(&arena);
+    switch (in->Command_case()) {
     case Request::CommandCase::kGet:
-        if (const auto it = kv.find(in.get().key()); it != kv.end()) {
-            out.mutable_got()->set_value(it->second);
+        if (const auto it = kv.find(in->get().key()); it != kv.end()) {
+            out->mutable_got()->set_value(it->second);
         } else {
             std::ostringstream os;
-            os << in.get().key() << " is not bound.";
-            out.mutable_fail()->set_message(std::move(os).str());
+            os << in->get().key() << " is not bound.";
+            out->mutable_fail()->set_message(std::move(os).str());
         }
         break;
     case Request::CommandCase::kSet: {
-        const auto& s = in.set();
+        const auto& s = in->set();
         const int n = s.mapping_size();
         for (int i = 0; i < n; ++i) {
             const auto& m = s.mapping(i);
             kv[m.key()] = m.value();
         }
-        out.mutable_sot();
+        out->mutable_sot();
         break;
     }
     default                        :
-        out.mutable_fail()->set_message("Unknown command");
+        out->mutable_fail()->set_message("Unknown command");
         break;
     }
 
-    return pb_serialize(dest, out);
+    return pb_serialize(dest, *out);
 }
