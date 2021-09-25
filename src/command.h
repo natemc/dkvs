8672f2327a1736488ea05f2e47a5f8e343808e0e
Nate McNamara <nate@mcnamara.net>
2021-09-25 16:09:17 -0400

    Restrict Command and Reply to uint8_t

--- src/command.h
+++ src/command.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <cstdint>
 #include <string_view>
 #include <utility>
 #include <vector>
 
-enum class Command { invalid, follow, get, set };
+enum class Command: uint8_t { invalid, follow, get, set };
+enum class Reply: uint8_t { invalid, fail, got, sot, recap };
 
 std::pair<Command, std::string_view> which_command(std::string_view command);
 
